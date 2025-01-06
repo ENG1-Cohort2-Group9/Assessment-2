b42a592e533cf01a5ce76fddabaaa8554a685b73
Kiyan <kiyandeano@gmail.com>
2025-01-06 00:32:06 +0000

    Building Capacities now displayed

--- game/core/src/main/java/io/github/archessmn/ENG1/GameModel/Objects/Use.java
+++ game/core/src/main/java/io/github/archessmn/ENG1/GameModel/Objects/Use.java
@@ -5,18 +5,21 @@ package io.github.archessmn.ENG1.GameModel.Objects;
  * certain use pairs with the added type should be weighted differently, as is already done in World.
  */
 public enum Use {
-    TEACHING("Teaching"),
-    ACCOMMODATION("Accommodation"),
-    CAFETERIA("Cafeteria"),
-    RECREATION("Recreation");
+    TEACHING("Teaching", "Teaching"),
+    ACCOMMODATION("Accommodation", "Accomm."),
+    CAFETERIA("Cafeteria", "Cafeteria"),
+    RECREATION("Recreation", "Recreation");
 
     private final String stringName;
+    private final String stringShortName;
 
-    Use(String stringName) {
+    Use(String stringName, String stringShortName) {
         this.stringName = stringName;
+        this.stringShortName = stringShortName;
     }
 
     public String getStringName() {
         return stringName;
     }
+    public String getStringShortName() { return stringShortName; }
 }
